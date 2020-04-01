8e2792a7aae80b8dc90fb936576292b76a02675a
zhuzeitou <zhuzeitou@gmail.com>
2020-04-01 16:24:11 +0800

    读取resources中资源

---
+++ src/main/kotlin/Resources.kt
@@ -0,0 +1,11 @@
+import java.nio.charset.Charset
+
+private val classLoader by lazy { object {}.javaClass.classLoader }
+
+fun String.asResource() = classLoader.getResource(this)
+
+fun String.asResourceBytes() = asResource().readBytes()
+
+fun String.asResourceText(charset: Charset = Charsets.UTF_8) = asResourceBytes().toString(charset)
+
+fun String.asResourceStream() = classLoader.getResourceAsStream(this)
