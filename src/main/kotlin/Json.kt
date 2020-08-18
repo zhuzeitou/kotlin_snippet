a3f14c80e937ba3d9700ee48d1ad792f4a65a0a3
zhuzeitou <zhuzeitou@gmail.com>
2020-08-18 14:42:41 +0800

    更新kotlin 1.4.0，移除ktor，使用okhttp

--- src/main/kotlin/Json.kt
+++ src/main/kotlin/Json.kt
@@ -1,37 +1,66 @@
 import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
 import kotlinx.serialization.modules.SerializersModule
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
-private val context = SerializersModule {
-    contextual(Date::class, DateSerializer)
-    contextual(Timestamp::class, TimeStampSerializer)
+val jsonObject = Json {
+    serializersModule = SerializersModule {
+        contextual(Date::class, DateSerializer2)
+        contextual(Timestamp::class, TimeStampSerializer)
+    }
+    ignoreUnknownKeys = true
 }
 
-val jsonObject = Json(JsonConfiguration.Stable.copy(ignoreUnknownKeys = true), context)
+inline fun <reified T> T.toJson(): String = jsonObject.encodeToString(this)
 
-@OptIn(ImplicitReflectionSerializer::class)
-inline fun <reified T> T.toJson(): String = jsonObject.stringify(serializer(), this)
+inline fun <reified T> String.parseJson(): T = jsonObject.decodeFromString(this)
 
-@OptIn(ImplicitReflectionSerializer::class)
-inline fun <reified T> String.parseJson(): T = jsonObject.parse(serializer(), this)
-
-typealias Date = @ContextualSerialization java.util.Date
+typealias Date = @Contextual java.util.Date
+typealias LongDate = @Serializable(with = DateSerializer::class) java.util.Date
 
 @Serializer(forClass = Date::class)
 object DateSerializer : KSerializer<Date> {
-    override val descriptor = PrimitiveDescriptor("Date", PrimitiveKind.LONG)
+    override val descriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.LONG)
 
     override fun deserialize(decoder: Decoder) = Date(decoder.decodeLong())
 
     override fun serialize(encoder: Encoder, value: Date) = encoder.encodeLong(value.time)
 }
 
+@Serializer(forClass = java.util.Date::class)
+object DateSerializer2 : KSerializer<java.util.Date> {
+    override val descriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
+
+    private val df: ThreadLocal<DateFormat> = object : ThreadLocal<DateFormat>() {
+        override fun initialValue(): DateFormat {
+            return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
+        }
+    }
+
+    override fun deserialize(decoder: Decoder) = java.util.Date(
+        LocalDateTime.parse(decoder.decodeString(), DateTimeFormatter.ISO_OFFSET_DATE_TIME).toInstant(ZoneOffset.UTC)
+            .toEpochMilli()
+    )
+
+    override fun serialize(encoder: Encoder, value: java.util.Date) = encoder.encodeString(
+        DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(
+            OffsetDateTime.ofInstant(value.toInstant(), ZoneOffset.UTC)))
+}
+
 typealias Timestamp = @Serializable(with = TimeStampSerializer::class) java.sql.Timestamp
 
 @Serializer(forClass = Timestamp::class)
 object TimeStampSerializer : KSerializer<Timestamp> {
-    override val descriptor = PrimitiveDescriptor("Timestamp", PrimitiveKind.LONG)
+    override val descriptor = PrimitiveSerialDescriptor("Timestamp", PrimitiveKind.LONG)
 
     override fun deserialize(decoder: Decoder) = Timestamp(decoder.decodeLong())
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,9 @@
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
-import io.ktor.client.request.forms.FormDataContent
-import io.ktor.client.request.header
-import io.ktor.client.request.post
-import io.ktor.client.request.url
-import io.ktor.http.Parameters
+
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import java.lang.Exception
 import java.net.URLEncoder
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
@@ -22,22 +17,21 @@ data class HttpBin<T>(val form: T)
 @OptIn(ExperimentalTime::class)
 fun main() {
     runBlocking {
-        val client = HttpClient(Apache) {
-            install(JsonFeature) {
-                serializer = KotlinxSerializer(jsonObject)
+        val client = OkHttpClient()
+        val request = okhttp3.Request.Builder()
+            .url("http://httpbin.org/post")
+            .post(FormBody.Builder()
+                .add("a", "1")
+                .add("b", "12345")
+                .build())
+            .header("isp", URLEncoder.encode("你好", "utf-8"))
+            .build()
+        client.newCall(request).execute().use { response ->
+            if (response.isSuccessful) {
+                response.body?.let { body ->
+                    println(body.string())
+                }
             }
-        }
-
-        closeablesOf(client).use {
-            val body = client.post<HttpBin<Form>> {
-                url("http://httpbin.org/post")
-                body = FormDataContent(Parameters.build {
-                    append("a", "1")
-                    append("b", "12345")
-                })
-                header("isp", URLEncoder.encode("你好", "utf-8"))
-            }.form
-            println(body)
         }
 
         val mark = TimeSource.Monotonic.markNow()
@@ -52,7 +46,6 @@ data class HelloJson<T>(val code: Int, val message: String, val data: T? = null)
 @Serializable
 data class HelloJson2(val a: Int, val b: Timestamp)
 
-@OptIn(ExperimentalStdlibApi::class)
 fun test() {
     val data = HelloJson(0, "success", listOf("aaa", "bbb", "ccc"))
 
